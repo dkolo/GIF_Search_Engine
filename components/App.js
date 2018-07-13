204fe2c8592dee91adf1faacb2896433475bcbe2
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-13 14:45:45 +0200

    Start adjusting the promise to the getGif function

--- components/App.js
+++ components/App.js
@@ -20,27 +20,32 @@ App = React.createClass({
         }.bind(this));
     },
 
-    getGif: function (searchingText, callback) {
-        return new Promise (
+    getGif: function (searchingText) {
+        return new Promise(
             function (resolve, reject) {
-                const  = newXMLHttpRequest();
-                request.onload = function() {
-                    if this.status===200) {
-                        resolve(this.response);
-                    } else {
+                const xhr = newXMLHttpRequest();
+                xhr.onload = function () {
+                    if xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText).data;
+                        var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        } else {
                         reject(new Error(this.statusText));
                     }
-                    };
-                    request.onerror = function () {
-                        reject(new Error (
-                            `XMLHttpRequest Error: ${this.statusText}`
-                        ));
-                    };
-                    request.open('GET')
-                }
+                };
+                xhr.onerror = function () {
+                    reject(new Error(
+                        `XMLHttpRequest Error: ${this.statusText}`
+                    ));
+                };
+                xhr.open('GET', url);
+                xhr.send();
+            }
             }
         )
         /*
+        getGif: function (searchingText, callback) {
         var GIPHY_API_URL = 'https://api.giphy.com';
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + 'AeMRGJvfOzX10nFqx1B7OdB9vUYxOZ7W' + '&tag=' + searchingText;
         var xhr = new XMLHttpRequest();
@@ -59,27 +64,27 @@ App = React.createClass({
         */
     },
 
-    render: function () {
+render: function () {
 
-        var styles = {
-            margin: '0 auto',
-            textAlign: 'center',
-            width: '90%'
-        };
+    var styles = {
+        margin: '0 auto',
+        textAlign: 'center',
+        width: '90%'
+    };
 
-        return (
-            <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-                <Search
-                    onSearch={this.handleSearch}
-                />
-                <Gif
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
-            </div>
-        );
-    }
+    return (
+        <div style={styles}>
+            <h1>Wyszukiwarka GIFow!</h1>
+            <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+            <Search
+                onSearch={this.handleSearch}
+            />
+            <Gif
+                loading={this.state.loading}
+                url={this.state.gif.url}
+                sourceUrl={this.state.gif.sourceUrl}
+            />
+        </div>
+    );
+}
 });
